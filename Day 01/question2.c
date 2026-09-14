edb0c9097abb18bd82ba5488ef5967b1151bbd6a
Ritvik Joshi <ritvikjoshi@Ritviks-MacBook-Air.local>
2026-09-14 15:17:10 +0530

    Day 01

---
+++ Day 01/question2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+int a,b,sum,difference,product,quotient;
+printf("enter the value of a");
+scanf("%d",&a);
+printf(" enter the value of b");
+scanf("%d",&b);
+sum =a+b;
+printf("sum is: %d\n",sum);
+difference= a-b;
+printf("difference is :%d\n",difference);
+product=a*b;
+printf("product is:%d\n",product);
+quotient=a/b;
+printf("quotient is:%d\n",quotient);
+return 0;
+}
